afd16b1ea14acd74bee3830f6e9df4d3a8386866
babyshoes <dianecai@gmail.com>
2015-08-04 18:41:44 -0400

    ladle on

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array_of_possibles)
+    anagrams_list = []
+    array_of_possibles.each do |possibility|
+      anagrams_list << possibility if possibility.split(//).sort == @word.split(//).sort 
+      # binding.pry
+    end
+    anagrams_list
+  end
+
+end
+
+allergy = Anagram.new("allergy")
+allergy.match(%w(gallery ballerina regally clergy largely leading))
